5003e4e4e1e225cde09be2d850c729d60fc2cb8b
Yubo Li <yubol@andrew.cmu.edu>
2025-11-09 08:42:14 -0500

    update search bar

--- assets/js/shortcut-key.js
+++ assets/js/shortcut-key.js
@@ -4,8 +4,8 @@ document.addEventListener("readystatechange", () => {
     let isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
     let shortcutKeyElement = document.querySelector("#search-toggle .nav-link");
     if (shortcutKeyElement && isMac) {
-      // use the unicode for command key
-      shortcutKeyElement.innerHTML = '&#x2318; k <i class="ti ti-search"></i>';
+      // Keep only the search icon, remove Command K text
+      shortcutKeyElement.innerHTML = '<i class="ti ti-search"></i>';
     }
   }
 });
